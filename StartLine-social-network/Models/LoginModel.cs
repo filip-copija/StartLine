9c1ec165d1eb97e035c1a072a25720481b7d1e43
Filip Copija <Filip.Copija@microsoft.wsei.edu.pl>
2022-12-27 13:32:11 +0100

    Add LoginModel

---
+++ StartLine-social-network/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StartLine_social_network.Models
+{
+    public class LoginModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is reuqired.")]
+        public string UserName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "This field is reuqired.")]
+        public string Password { get; set; }
+    }
+}
