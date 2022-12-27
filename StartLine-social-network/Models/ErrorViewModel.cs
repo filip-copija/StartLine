6c3c65e9e11c7095b0b22f804ded84831b94f937
Filip Copija <Filip.Copija@microsoft.wsei.edu.pl>
2022-12-27 12:21:22 +0100

    Dodaj pliki projektów.

---
+++ StartLine-social-network/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StartLine_social_network.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
