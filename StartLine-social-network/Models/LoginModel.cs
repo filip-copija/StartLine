21bb0ef7f4413e67826d8bb691a85ba7249e0569
Filip Copija <Filip.Copija@microsoft.wsei.edu.pl>
2022-12-27 13:35:22 +0100

    Update LoginModel

--- StartLine-social-network/Models/LoginModel.cs
+++ StartLine-social-network/Models/LoginModel.cs
@@ -1,13 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace StartLine_social_network.Models
 {
     public class LoginModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "This field is reuqired.")]
+        [DisplayName("Login")]
         public string UserName { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "This field is reuqired.")]
+        [DisplayName("Password"), DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
