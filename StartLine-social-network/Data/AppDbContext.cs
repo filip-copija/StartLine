a9141b5b1a9cb2b201788b9b4155b9a3ee7478dd
Filip Copija <filip.copija1@gmail.com>
2023-01-22 19:00:21 +0100

    Add AppDbContext, EntityFrameworkCore

---
+++ StartLine-social-network/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StartLine_social_network.Models;
+
+namespace StartLine_social_network.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Party> Parties { get; set; }
+        public DbSet<Address> Addresses { get; set;  }
+    }
+}
