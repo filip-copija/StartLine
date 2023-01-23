f834d137dd3655a2921a32404f64e905629d2231
Filip Copija <filip.copija1@gmail.com>
2023-01-23 15:24:27 +0100

    Add PartyController.cs, ClubController.cs & Club/Party views

---
+++ StartLine-social-network/Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StartLine_social_network.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StartLine-social-network/Controllers/PartyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StartLine_social_network.Controllers
+{
+    public class PartyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
