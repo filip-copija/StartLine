54865a8bf2aa8e7740eb555ac9c1d0f4dcc36408
Filip Copija <filip.copija1@gmail.com>
2023-01-23 20:21:20 +0100

    Update PartyController.cs, Index.cshtml

--- StartLine-social-network/Controllers/PartyController.cs
+++ StartLine-social-network/Controllers/PartyController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StartLine_social_network.Data;
+using StartLine_social_network.Models;
 
 namespace StartLine_social_network.Controllers
 {
     public class PartyController : Controller
     {
+        private readonly AppDbContext _context;
+        public PartyController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Party> clubs = _context.Parties.ToList();
+            return View(clubs);
         }
     }
 }
