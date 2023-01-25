821ef85fe054aa9d04ed7d04c1f5b8dd277a8089
Filip Copija <filip.copija1@gmail.com>
2023-01-25 07:40:16 +0100

    Authentication 1

---
+++ StartLine-social-network/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StartLine_social_network.Data;
+using StartLine_social_network.Models;
+using StartLine_social_network.ViewModels;
+
+namespace StartLine_social_network.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly AppDbContext _context;
+        public AccountController(UserManager<AppUser> userManager,
+                                 SignInManager<AppUser> signInManager,
+                                 AppDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Login()
+        {
+            var response = new LoginViewModel();
+            return View(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid) return View(loginVM);
+
+            var user = await _userManager.FindByEmailAsync(loginVM.Username);
+
+            if (user != null)
+            {
+                // User exists, checking password here
+                var passCheck = await _userManager.CheckPasswordAsync(user, loginVM.Password);
+                if (passCheck)
+                {
+                    // Password correcnt, signing in
+                    var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Party"); // Action name, Controller name
+                    }
+                }
+                // Password incorrect
+                TempData["Error"] = "Login or password is incorrect. Please try again";
+                // Try again
+                return View(loginVM);
+            }
+            // Not found
+            TempData["Error"] = "Login or password is incorrect. Please try again";
+            return View(loginVM);
+        }
+    }
+}
---
+++ StartLine-social-network/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StartLine_social_network.ViewModels
+{
+    public class LoginViewModel
+    {      
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string Username { get; set; }
+        [Display(Name = "Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
